f494d643ab8c9c4948a447e37f09c35f661f8c55
UjjawalChoudhary912 <ujjawalchoudhary912@gmail.com>
2024-03-21 18:39:07 +0530

    Authentication hook for login and custom service hook for showing alert message to users

--- eshop-app/src/App.js
+++ eshop-app/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './common/Navbar';
+import useAuthentication from './hooks/useAuthentication';
+import useServices from './hooks/useServices';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <useAuthentication/>
+      <useServices/>
     </div>
   );
 }
