ca46dd60297ed5a99875b6eca19f4ef7e9ffa363
UjjawalChoudhary912 <ujjawalchoudhary912@gmail.com>
2024-03-21 20:56:28 +0530

    Login Page, Sign up Page, navigation

--- eshop-app/src/App.js
+++ eshop-app/src/App.js
@@ -1,18 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './common/Navbar';
-import useAuthentication from './hooks/useAuthentication';
-import useServices from './hooks/useServices';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import PageSetUp from "./components/pageSetUp/PageSetUp";
+import {useDispatch} from "react-redux";
+import {useCallback, useEffect} from "react";
+import {initCatalog} from "./store/actions/metadataAction";
 
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#3f51b5',
+		},
+		secondary: {
+			main: '#f50057',
+		},
+		disabled: {
+			main: "#56595c",
+		}
+	},
+});
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <useAuthentication/>
-      <useServices/>
-    </div>
-  );
+	const dispatch = useDispatch();
+	const initPageData = useCallback((dispatchEvent = dispatch) => {
+		dispatchEvent(initCatalog());
+	}, [dispatch]);
+	useEffect(() => {
+		initPageData();
+	}, [initPageData]);
+	return (
+		<ThemeProvider theme={theme}>
+			<PageSetUp />
+		</ThemeProvider>
+	);
 }
 
 export default App;
